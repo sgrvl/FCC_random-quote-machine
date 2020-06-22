5cd31c2e1ea674fe1966026978926c71b75e7d80
BuildTools <unconfigured@null.spigotmc.org>
2020-06-22 02:19:30 -0400

    Free code camp project - random quote machine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import quotes from "./quotes.json";
+import twitter from "./icons8-twitter.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			randomNum: Math.floor(Math.random() * 102),
+			randomColor: Math.floor(Math.random() * 16777215).toString(16),
+		};
+		this.newQuote = this.newQuote.bind(this);
+	}
+
+	newQuote() {
+		this.setState({
+			randomNum: Math.floor(Math.random() * 102),
+			randomColor: Math.floor(Math.random() * 16777215).toString(16),
+		});
+	}
+
+	render() {
+		document.documentElement.style.setProperty(
+			"--color",
+			`#${this.state.randomColor}`
+		);
+
+		const tweet = `"${quotes[this.state.randomNum].quote}" ${
+			quotes[this.state.randomNum].author
+		}`;
+
+		return (
+			<div className="app">
+				<div id="quote-box">
+					<div id="text">"{quotes[this.state.randomNum].quote}"</div>
+					<div id="author">- {quotes[this.state.randomNum].author}</div>
+					<div id="actions">
+						<a
+							href={`https://twitter.com/intent/tweet?text=${tweet}&hashtags=quotes`}
+							id="tweet-quote"
+							target="_blank"
+						>
+							<img src={twitter} alt="twitter icon" />
+						</a>
+						<button id="new-quote" onClick={this.newQuote}>
+							New Quote
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
